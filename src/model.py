Fixed velocity calculation

src/model.py
@@ -51,7 +51,7 @@ class NSModel:
         return counter
 
     def lane_velocity(self):
-        return np.array([lane[lane != -1].mean() for lane in self.highway])
+        return np.array([lane[0 <= lane].mean() for lane in self.highway])
 
     def highway_velocity(self):
         return self.lane_velocity().mean()

src/validation.py
@@ -114,7 +114,7 @@ def cars_per_site(steps=200, model=NSModel):
 
 def velocity_to_density_lanes(delta=0.01, steps=100, prob=0.5, model=NSModel):
     mean_velocity = {"1": [], "2": [], "3": [], "4": []}
-    densities = np.arange(0.01, 1 + delta, delta)
+    densities = np.arange(0, 1 + delta, delta)
 
     for lane in mean_velocity.keys():
         for density in densities: